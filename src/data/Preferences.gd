66e59f28a7ba53b2b6c22707155a0c2214643690
Lukasdotcom <lukas.aurelius@outlook.com>
2021-10-08 09:52:51 -0400

    Fixed bug with backwards compatible saves.

--- src/data/Preferences.gd
+++ src/data/Preferences.gd
@@ -1,53 +1,77 @@
 extends Node
 
-export(Dictionary) var preferences: = {
-	"player" : {
-		"brake" : -600,
-		"accelerate" : 600,
-		"rotation" : 2,
-		"reload" : 1000
+const preference_file = "user://preferences.json"
+
+
+export(Dictionary) var startPreference: = {
+	"bullet": {
+		"speed": 700
 	},
-	"enemy" : {
-		"AI" : [
+	"enemy": {
+		"AI": [
 			{
-				"action" : "accelerate",
-				"strength" : 1,
-				"time" : 3000,
-				"variance" : 100
+				"action": "accelerate",
+				"strength": 1,
+				"time": 3000,
+				"variance": 100
 			},
 			{
-				"action" : "brake",
-				"strength" : 0,
-				"time" : 1000,
-				"variance" : 100
+				"action": "brake",
+				"strength": 0,
+				"time": 1000,
+				"variance": 100
 			}
 		],
-		"brake" : -500,
-		"accelerate" : 500,
-		"rotation" : 1.7,
-		"reload" : 1200,
-		"reloadConsistency" : 100,
-		"bannedSpawn" : [
-			[200, 1720, 200, 880]
-		]
+		"accelerate": 500,
+		"bannedSpawn": [
+			[
+				200,
+				1720,
+				200,
+				880
+			]
+		],
+		"brake": -500,
+		"reload": 1200,
+		"reloadConsistency": 100,
+		"rotation": 1.7
 	},
-	"friction" : 0.2,
-	"scale" : 1,
-	"bullet" : {
-		"speed" : 700
+	"friction": 0.2,
+	"player": {
+		"accelerate": 600,
+		"brake": -600,
+		"reload": 1000,
+		"rotation": 2
 	},
-	"waitAfterDeath" : 1
-} setget changed
+	"scale": 1,
+	"waitAfterDeath": 1
+} 
+export var preferences: Dictionary = startPreference.duplicate() setget changed
+
+func _ready() -> void:
+	var file = File.new()
+	if file.file_exists(preference_file):
+		file.open(preference_file, File.READ)
+		var data = parse_json(file.get_as_text())
+		file.close()
+		if typeof(data) == TYPE_DICTIONARY:
+			preferences = data
+
 
 func changed(value: Dictionary) -> void:
 	preferences = correctValues(preferences, value, [])
+	var file = File.new()
+	file.open(preference_file, File.WRITE)
+	file.store_string(to_json(preferences))
+	file.close()
 
 func correctValues(old: Dictionary, new: Dictionary, treePath: Array) -> Dictionary:
-	for x in new:
+	for x in old:
 		if typeof(old[x]) == TYPE_DICTIONARY:
-			var newTreePath = treePath.duplicate()
-			newTreePath.append(x)
-			correctValues(old[x], new[x], treePath)
+			if x in new:
+				var newTreePath = treePath.duplicate()
+				newTreePath.append(x)
+				old[x] = correctValues(old[x], new[x], treePath)
 		else:
 			old[x] = new[x]
 	return old

aa41192b33da4063812b2f144bc9463ce1a143d7
Lukasdotcom <lukas.aurelius@outlook.com>
2021-10-16 12:39:37 -0400

    Added Credits

--- src/Menu/Start.gd
+++ src/Menu/Start.gd
@@ -11,3 +11,7 @@ func _on_Preferences_button_up() -> void:
 
 func _on_Quit_button_up() -> void:
 	get_tree().quit()
+
+
+func _on_Credits_button_up() -> void:
+	get_tree().change_scene("res://src/Menu/Credits.tscn")

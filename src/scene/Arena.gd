0cc704d1047c2b410d17d429bebeae21d2a24286
Lukasdotcom <lukas.aurelius@outlook.com>
2021-10-29 20:19:35 -0400

    Adjusts the aspect ratio of the game to fit the players screen

--- src/scene/Arena.gd
+++ src/scene/Arena.gd
@@ -1,5 +1,12 @@
 extends Node2D
+onready var _tile_map: TileMap = $TileMap
 
+func _ready() -> void: # Adjusts the aspect ratio of the map so it fits correctly with user aspect ratio
+	var _new_scale = get_viewport().size
+	if _new_scale.x / _new_scale.y * 1080 / 1920 > 1:
+		_tile_map.set_scale(Vector2(_new_scale.x / 1920 * 1080 / _new_scale.y, 1))
+	else:
+		_tile_map.set_scale(Vector2(1, _new_scale.y / 1080 * 1920 / _new_scale.x))
 
 
 func on_player_death() -> void:

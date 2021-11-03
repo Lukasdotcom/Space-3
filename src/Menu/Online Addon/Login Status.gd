b4caacdc176379d54858d6577ced970f2c19195f
Lukasdotcom <lukas.aurelius@outlook.com>
2021-11-03 16:54:42 -0400

    Adds a logout

--- src/Menu/Online Addon/Login Status.gd
+++ src/Menu/Online Addon/Login Status.gd
@@ -5,9 +5,11 @@ onready var status: Label = $Status
 
 func _ready() -> void: # Checks if the user is logged in
 	if Preferences.userName:
-		loginButton.queue_free()
+		loginButton.set_text("Logout")
 		status.set_text("Logged in as " + Preferences.userName)
 
 
-func _on_Button_button_up() -> void: # Goes to sign in screen
+func _on_Button_button_up() -> void: # Goes to sign in screen and logs out if not signed in
+	Preferences.userName = ""
+	Preferences.key = ""
 	get_tree().change_scene("res://src/Menu/Online Addon/Sign in.tscn")
